312928d03908ceeb02a6e5b87bd7463a927b53e8
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-07-11 23:18:16 +0300

    Начало

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Java, NetHran.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 20.05.2019
+ * @link https://github.com/Centnerman
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("lksdhnlkdb");
+    }
+}
---
+++ src/main/java/ru/vlsv/common/AbstractMessage.java
@@ -0,0 +1,6 @@
+package ru.vlsv.common;
+
+import java.io.Serializable;
+
+public abstract class AbstractMessage implements Serializable {
+}
