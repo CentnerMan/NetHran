ab450eb4d3825e763bcdaf622df591622ef77a51
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-07-21 23:53:18 +0300

    .

--- src/main/java/ru/vlsv/client/MainController.java
+++ src/main/java/ru/vlsv/client/MainController.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URL;
@@ -29,6 +28,7 @@ public class MainController implements Initializable {
     ListView<String> remoteFilesList;
 
     private static final String LOCAL_STORAGE = "client_storage";
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
